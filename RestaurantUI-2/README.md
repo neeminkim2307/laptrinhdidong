1f3d52c092bbbac14a8fb4ed7f960e81969574a8
Khiem Nguyen <khiemnguyenminh230@gmail.com>
2026-03-19 15:38:58 +0700

    [#2] Update Restaurant

---
+++ RestaurantUI-2/README.md
@@ -0,0 +1,10 @@
+## **Họ tên: Nguyễn Minh Khiêm
+## **MSV: 23810310305
+## **Lớp: D18CNPM4
+
+DEMO:
+<img src="../screenshots/th2-1.jpg" width=200>
+<img src="../screenshots/th2-2.jpg" width=200>
+<img src="../screenshots/th2-3.jpg" width=200>
+<img src="../screenshots/th2-4.jpg" width=200>
+<img src="../screenshots/th2-5.jpg" width=200>
---
+++ RestaurantUI-2/data.js
@@ -0,0 +1,45 @@
+// data.js
+export const foodList = [
+  {
+    id: '1',
+    name: 'BURGER',
+    price: '$28',
+    rating: '4.9 (3k+ Rating)',
+    image: require('./assets/burgerr.png') 
+  },
+  {
+    id: '2',
+    name: 'PIZZA',
+    price: '$35',
+    rating: '4.7 (2k+ Rating)',
+    image: require('./assets/pizza.png') 
+  },
+  {
+    id: '3',
+    name: 'COCA COLA',
+    price: '$5',
+    rating: '4.5 (1k+ Rating)',
+    image: require('./assets/coca.jpg') 
+  },
+  {
+    id: '4',
+    name: 'FRIED RICE',
+    price: '$15',
+    rating: '4.8 (5k+ Rating)',
+    image: require('./assets/friedrice.jpg') 
+  },
+  {
+    id: '5',
+    name: 'French Fries',
+    price: '$8',
+    rating: '4.8 (5k+ Rating)',
+    image: require('./assets/frenchfries.jpg') 
+  },
+  {
+    id: '6',
+    name: 'Steak',
+    price: '$8',
+    rating: '4.8 (5k+ Rating)',
+    image: require('./assets/steak.jpg') 
+  }
+];
